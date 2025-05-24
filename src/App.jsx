fa298ea9d994dd423f3504451fd1655e2ac9d1fb
Christina-888 <christinabykadorova@gmail.com>
2025-05-24 11:29:56 +0300

    add superheroes gallery

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+//import { useState } from 'react'
+import './App.css'
+import Header from './components/Header'
+import Heroes from './components/Cards'
+
+function App() {
+  return (
+    <>
+      <div>
+        <Header />
+        <Heroes />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>ГАЛЕРЕЯ СУПЕРГЕРОЕВ</h1>
+    </header>
+  );
+}
+
+export default Header;
